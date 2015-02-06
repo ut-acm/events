1e51e0000876cb090611836e26690bbfee550c4f
Feri73 <ferisampad@gmail.com>
2015-02-06 18:42:33 +0330

    f3

--- db/migrate/20150206155025_faraz.rb
+++ db/migrate/20150206155025_faraz.rb
@@ -1,7 +1,7 @@
 class Faraz < ActiveRecord::Migration
   def change
-	add_column :prfiles, :phone_number, :string
-	add_column :prfiles, :rating, :integer,:default => 0
+	add_column :profiles, :phone_number, :string
+	add_column :profiles, :rating, :integer,:default => 0
 	add_column :events, :report_id, :integer
   end
 end
