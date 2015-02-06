4b8ee5d4aee46bfc1f444b1f273a6cd02052e244
Feri73 <ferisampad@gmail.com>
2015-02-06 18:58:43 +0330

    f5

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'sprockets/railtie'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Bundler.require(:default, :assets, Rails.env)
 
 module Acm
   class Application < Rails::Application
