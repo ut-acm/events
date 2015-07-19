5be46e04340bea19b299f4467db1be043470e28b
Feri73 <ferisampad@gmail.com>
2015-07-19 20:02:57 +0430

    coupon

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -25,7 +25,7 @@ class Participation < ActiveRecord::Base
 
     def create_enroll_token
       loop do
-        a=SecureRandom.urlsafe_base64
+        a=SecureRandom.urlsafe_base64(3)
         return a if Participation.where(:enroll_token=>a,:event=>self.event).size==0
       end
     end
