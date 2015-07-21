18ee498b1123debc6ca14df3f4f3dc228c5f5de9
Feri73 <ferisampad@gmail.com>
2015-07-21 10:59:38 +0430

    auth in controllers

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,6 +1,5 @@
 class CouponsController < ApplicationController
   before_action :set_coupon, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:show, :edit, :update, :destroy,:fil]
   before_action :authenticate_admin
 
   # GET /coupons
