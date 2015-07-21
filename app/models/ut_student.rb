50b957a93729c8fd5d00531bd32732aa119ff601
Feri73 <ferisampad@gmail.com>
2015-07-21 15:03:12 +0430

    auth in controllers

--- app/models/ut_student.rb
+++ app/models/ut_student.rb
@@ -2,6 +2,7 @@
 class UtStudent < ActiveRecord::Base
 	belongs_to :user
 	validate :ut_mail
+	validates_uniqueness_of :email
 
 	def ut_mail
 		self.errors.add(:email=>"ایمیل دانشگاه را وراد کنید.") unless self.email.ends_with?("@ut.ac.ir")
