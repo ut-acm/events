42c066afc6e69725215a23e9c998e466b861893c
Feri73 <ferisampad@gmail.com>
2015-07-21 20:04:01 +0430

    auth in controllers

--- app/controllers/ut_students_controller.rb
+++ app/controllers/ut_students_controller.rb
@@ -49,8 +49,8 @@ class UtStudentsController < ApplicationController
     respond_to do |format|
       if @ut_student.save
         UserMailer.send_ut_validation(current_user).deliver
-        flash[:notice] ='ایمیل شما ثبت شد و ایمیلی جهت تایید به شما داده شد.'
-        format.html { redirect_to events_path}
+        
+        format.html { redirect_to events_path, notice: 'ایمیل شما ثبت شد و ایمیلی جهت تایید به شما داده شد.' }
         format.json { render :show, status: :created, location: @ut_student }
       else
         format.html { render :new }
