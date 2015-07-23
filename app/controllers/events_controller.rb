958e9a2f4ad661a56df9a0469ac20411b0772e69
Feri73 <ferisampad@gmail.com>
2015-07-23 18:27:32 +0430

    auth

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -134,9 +134,10 @@ end
   end
 
   def check_token
+    return
     par=Participation.where(:event=>self,:enroll_token=>params[:token],:enroll_seen=>[nil,false]).first
     @pass=par
-    par.update(:enroll_seen=>true) if par
+    # par.update(:enroll_seen=>true) if par
   end
 
 
