3eef1e4dd261485d38dfff36b0f36ab7eac05992
Feri73 <ferisampad@gmail.com>
2015-09-23 09:11:21 +0330

    ignore it!

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -50,7 +50,7 @@ class EventsController < ApplicationController
 
   def filter_by_category
     category = params[:category]
-    if category == 'Class' or category == 'Ring' or category == 'Lab' or category == 'Workshop' or category=='Conference' or category=='Talk'
+    if category == 'Class' or category == 'Ring' or category == 'Lab' or category == 'Workshop' or category=='Conference' or category=='Talk' or category=='Product'
       @events = Event.where("category = ?", category).where("begins > ?", Time.now).order(:begins)
       @archives = Event.where("category = ?", category).where("begins < ?", Time.now).order(:begins)
       @title = "رویدادها"
@@ -66,6 +66,8 @@ class EventsController < ApplicationController
         @title = "همایش ها"
       elsif category == 'Talk'
         @title = 'تاک ها'
+      elsif category == 'Product'
+        @title = 'محصولات قابل خرید'
       end
       render 'events/index'
     else
@@ -357,7 +359,7 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:title, :sentence, :description, :summary, :begins, :venue, :category, :capacity, :price, :poster,:tag_list,:is_conference_like, :officer_ids => [])
+    params.require(:event).permit(:title, :sentence, :description, :summary, :begins, :venue, :category, :capacity, :price, :poster,:tag_list,:is_conference_like,:product_address, :officer_ids => [])
     # params.permit!
   end
 
